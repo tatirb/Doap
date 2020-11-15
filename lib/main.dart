35429eee94e7fcb418e03957fc5fd5f0bbf30b47
tatirb <33660799+tatirb@users.noreply.github.com>
2020-11-15 03:54:27 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Wrapper(),
+      debugShowCheckedModeBanner: false,
       routes: {
         '/user-profile': (_) => UserProfile(),
       },

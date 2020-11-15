a1aaa1637657c76055c3429104238fbab7d7579f
tatirb <33660799+tatirb@users.noreply.github.com>
2020-11-15 03:49:05 -0300

    Update user_profile.dart

--- lib/Screen/user_profile.dart
+++ lib/Screen/user_profile.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
-//import 'package:doap/Screen/Home/main_drawer.dart';
+import 'package:doap/Services/database.dart';
+import 'package:provider/provider.dart';
+import 'package:doap/Screen/user_list.dart';
+import 'package:doap/Models/profile.dart';
 
 class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return StreamProvider<Profile>.value(
+      value: DatabaseService().user,
+      child: Scaffold(
       appBar: AppBar(
         title: Text('Perfil'),
       ),
-      //drawer: MainDrawer(),
+      body: UserList(),
+      ),
     );
   }
 }
